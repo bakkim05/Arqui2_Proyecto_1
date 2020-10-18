361e3edc0654f316c00dd09f487eb37abd62400b
Jung <bakkim05@gmail.com>
2020-10-17 23:13:41 -0600

    update

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 from Processor import Processor
+from Memory import Memory
+import threading
+import time
 
-p = Processor(5)
-p.writeCache('1001','ffff')
+
+if __name__ == "__main__":
+
+    processor = []
+    for i in range(4):
+        processor.append(Processor(i,1))
+    m = Memory(2)
+
+    processor[0].writeCache('1001','ffff')
+    print(processor[0].readCache('1001'))
